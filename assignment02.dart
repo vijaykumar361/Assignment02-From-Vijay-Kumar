b4d355ab655808f02643cc628a29d6b063d583eb
Vijay Kumar <95156170+vijaykumar361@users.noreply.github.com>
2021-12-08 10:10:51 +0500

    Add files via upload

---
+++ assignment02.dart
@@ -0,0 +1,58 @@
+// import 'dart:io';
+
+// void main(List<String> args) {
+//   // print(" ");
+//   // print("Task_01");
+//   // String Name = "Dart Course";
+//   // print(Name[0].toUpperCase() + Name.substring(1).toLowerCase());
+//   // print(Name.toUpperCase());
+//   // print(Name.toLowerCase());
+
+// //   // print(" ");
+// //   // print("Task_02");
+// //   // int a = 9;
+// //   // int add = 5;
+// //   // int multi = 10;
+// //   // double dvd = 2;
+// //   // double result = 9 + 5 * 10 / 2;
+// //   // print("Total Result:_$result");
+
+// //   // print(" ");
+// //   // print("Task_03");
+// //   // var a = 2;
+// //   // var b = 1;
+// //   // var result = --a - --b + ++b + b--;
+// //   // print("Result Is:_$result");
+
+//   // print(" ");
+//   // print("Task_04");
+//   // String name = "Vijay";
+//   // print("Name: $name");
+//   // String sub = "Dart";
+//   // print("Subject: $sub");
+//   // print(name.compareTo(sub));
+
+//   // print(" ");
+//   // print("Task_05");
+//   // print("Find Length");
+//   // print("Enter Your Name: ");
+//   // var name = stdin.readLineSync();
+//   // print("Length Is:${name!.length}");
+
+//   // print(" ");
+//   // print("Task_06");
+//   // print("Ascii Code");
+//   // String name = "VIJAY";
+//   // print(name.codeUnits);
+//   // print(name.codeUnitAt(2));
+
+//   //  print(" ");
+//   // print("Task_07");
+
+//   print(" ");
+//   print("Task_08");
+//   String name = "Vijay Kumar";
+//   print("Before Split: $name");
+//   print("After Split: ");
+//   print(name.split("Kumar"));
+// }
